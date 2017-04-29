7c45f0c3feea6aa3bb949f86a5d9a31de958c5d5
emaohi <emaohi@gmail.com>
2017-04-29 14:25:25 +0300

    changed permissions of managers and users

--- log/admin.py
+++ log/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
 from .models import *
-admin.site.register(EmployeeProfile)
 admin.site.register(Business)
+
+
+class EmployeeProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "role")
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_super_user():
+            # allow superusers to edit all profiles
+            return True
+        if obj is None:
+            # The docs say that the method should handle obj=None
+            # Don't allow user to edit profiles in general
+            return False
+        # Let the user edit the package if they are the owner.
+        return obj.user == request.user
+
+admin.site.register(EmployeeProfile, EmployeeProfileAdmin)
