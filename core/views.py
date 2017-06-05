8f8be67ad55e71bd5848fc59f4fa22a25bbbd621
Segev <rsegev@Administrators-MacBook-Pro-2.local>
2017-06-05 10:16:16 +0300

    pass also curr field val to incorrect report

--- core/views.py
+++ core/views.py
@@ -9,10 +9,12 @@ def report_incorrect_detail(request):
         reporting_profile = request.user.profile
         incorrect_field = request.POST.get('incorrect_field')
         fix_suggestion = request.POST.get('fix_suggestion')
+        curr_val = request.POST.get('curr_val')
 
         new_msg = Message(sender=reporting_profile, sent_time=timezone.now(), subject='Incorrect Employee Data',
-                          text='%s have mentioned that his %s field is incorrect; His suggestion is %s' %
-                               (reporting_profile.user.username, incorrect_field, fix_suggestion))
+                          text='%s have mentioned that his %s field is incorrect.'
+                               ' Current field value is %s; His suggestion is %s' %
+                               (reporting_profile.user.username, incorrect_field, curr_val, fix_suggestion))
         new_msg.save()
         # add the employee's manager to the recipients list
         new_msg.recipients.add(reporting_profile.get_manager())
