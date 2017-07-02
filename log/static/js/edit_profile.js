6960ca7a481e0fc25353cf04486927223665346c
Segev <rsegev@Administrators-MacBook-Pro-2.local>
2017-07-02 12:06:51 +0300

    started to move js code to dedicated js files

---
+++ log/static/js/edit_profile.js
@@ -0,0 +1,72 @@
+/**
+ * Created by rsegev on 29/06/2017.
+ */
+$(document).ready(function () {
+  $.ajax({
+      url: edit_profile_form_url, //from template
+      type: "get", //send it through get method,
+      success: form_success_callback,
+      error: function(xhr) {
+        //Do Something to handle error
+      }
+  });
+
+  $('#form_here').on('click', '.btn-warning', function() {
+      show_text_and_send_btn($(this));
+  });
+  $('#form_here').on('click', '.btn-info', function() {
+      var field_name = $(this).prev().prev().attr('name');
+      var fix_text = $(this).siblings('input[class="new_text"]').val();
+
+      var curr_val = "";
+      var $prevprev = $(this).prev().prev();
+
+      if ($prevprev.is('input')){
+          curr_val = $prevprev.attr('value');
+      }else if ($prevprev.is('select')){
+          curr_val = $prevprev.find(":selected").text();;
+      }
+      report_incorrect($(this), field_name, fix_text, curr_val);
+  });
+});
+
+function show_text_and_send_btn($elem) {
+    $elem.before('<input type="text" class="new_text" style="margin-left: 30px" placeholder="submit your fix">');
+    $elem.addClass('btn-info').removeClass('btn-warning').text('send');
+    $elem.after('<i class="fa fa-spinner fa-spin" style="font-size:18px; margin-left: 10px; display: none"></i>')
+}
+
+function form_success_callback(response) {
+                $('#form_here').html(response);
+
+                $('input:disabled').after(
+                    '<button type="button" class="btn btn-warning btn-xs report-btn">' +
+                    'Incorrect?</button>');
+                $('select:disabled').after(
+                    '<button type="button" class="btn btn-warning btn-xs report-btn">' +
+                    'Incorrect?</button><hr>');
+  }
+
+function report_incorrect($btn, field, fix, curr_val) {
+   var $spinner = $btn.siblings('.fa-spinner');
+   $spinner.show();
+    $.ajax({
+      url: report_incorrect_url, //from template
+      type: "post", //send it through get method
+      data: {
+        incorrect_field: field,
+        fix_suggestion: fix,
+        curr_val: curr_val
+      },
+      headers: {
+          'X-CSRFToken': csrf_token //from template
+      },
+      success: function(response){
+                $btn.siblings('input[class="new_text"]').remove();
+                $btn.after('<p style="display: inline; margin-left: 10px"> <b> '+ response + '</b> </p>');
+                $btn.remove();
+                $spinner.hide();
+
+            }
+    });
+}
---
+++ log/static/js/manage_employees.js
@@ -0,0 +1,116 @@
+/**
+ * Created by rsegev on 02/07/2017.
+ */
+
+$(document).ready(function () {
+
+    var curr_modal_username = "";
+
+    handle_manager_row();
+
+    $('.btn-danger').click(function () {
+        curr_modal_username = $(this).parents('td').siblings(":first").text();
+        show_delete_modal(curr_modal_username);
+    });
+    $('.btn-primary').click(function () {
+        curr_modal_username = $(this).parents('td').siblings(":first").text();
+        show_edit_modal(curr_modal_username);
+    });
+
+    $('#del_btn').click(function () {
+        del_user(curr_modal_username);
+    });
+
+    $('#broadcast_button').click(function () {
+        show_broadcast_modal();
+    });
+
+    $('.btn-submit').on('click', function() {
+        $('.btn-submit').hide();
+        $('.progress').show();
+        setTimeout(doProgress, 200);
+    });
+});
+
+function handle_manager_row() {
+    var $manager_row  = $('.manager-cell').parents('tr');
+    $manager_row.css('background-color', 'lightgreen');
+    $manager_row.find('.btn-xs').hide();
+}
+
+function show_edit_modal(username) {
+    $.ajax({
+      url: edit_profile_form_url,
+      type: "get", //send it through get method
+      data: {
+        username: username
+      },
+      success: function(response) {
+                    $('#edit').find('.modal-body').html(response);
+                    $('#edit').find('#Heading').html('Edit ' + username + ' details');
+                    $('#edit').modal('show');
+      },
+      error: function(xhr) {
+        //Do Something to handle error
+      }
+    });
+}
+
+function show_delete_modal(username){
+    $('#delete').find('h4').text("Delete " + username + " from your business");
+    $('#delete').modal('show');
+}
+
+function show_broadcast_modal(username) {
+    $.ajax({
+      url: broadcast_msg_url,
+      type: "get", //send it through get method
+      data: {
+      },
+      success: function(response) {
+                    $('#broadcast').find('.modal-body').html(response);
+                    $('#broadcast').find('#Heading').html('Broadcast message to all your employees');
+                    $('#broadcast').modal('show');
+      },
+      error: function(xhr) {
+        //Do Something to handle error
+      }
+    });
+}
+
+function del_user(username) {
+    $.ajax({
+      url: delete_user_url,
+      type: "post", //send it through get method
+      data: {
+        username: username
+      },
+      headers: {
+          'X-CSRFToken': csrf_token
+      },
+      success: function(response){
+                window.location.reload();
+            }
+    });
+}
+function doProgress() {
+    var employeeIndex = employees_length;
+    var i = 0;
+    var counterBack = setInterval(function(){
+      i++;
+      if (i <= 100){
+        $('.progress-bar').css('width', i+'%');
+      } else {
+        clearInterval(counterBack);
+      }
+      var is_plural_mail;
+      if (employeeIndex > 0){
+          is_plural_mail = "messages";
+      }else{
+          is_plural_mail = "message";
+      }
+      $("#percent").text((employeeIndex) + " " + is_plural_mail);
+
+    }, (employeeIndex+1) * 10);
+
+}
