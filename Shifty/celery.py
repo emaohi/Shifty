80be256027c472832df2ca3aabaa450f12aa266a
rsegev <rsegev@agtinternational.com>
2017-08-29 14:17:43 +0300

    fix celery conf

--- Shifty/celery.py
+++ Shifty/celery.py
@@ -1,5 +1,9 @@
 from __future__ import absolute_import, unicode_literals
+import os
 from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.dev')
 
 app = Celery(str('Shifty'))
 
