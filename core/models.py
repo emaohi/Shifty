be8680d7cd3a1c97b89c13e952a6dc25a010625d
rsegev <rsegev@agtinternational.com>
2017-11-06 14:08:05 +0200

    fix texts

--- core/models.py
+++ core/models.py
@@ -92,9 +92,9 @@ class ShiftSlot(models.Model):
     name = models.CharField(blank=True, null=True, default='Custom', max_length=30)
 
     def __str__(self):
-        return '%s slot - %s, %s to %s' %\
+        return '%s slot - %s, %s to %s%s' %\
                (self.name, self.get_day_str(), str(self.start_hour),
-                str(self.end_hour) + ' MANDATORY' if self.is_mandatory else '')
+                str(self.end_hour), ' --MANDATORY--' if self.is_mandatory else '')
 
     def start_time_str(self):
         return '%s %s' % (self.get_date(), self.start_hour)
