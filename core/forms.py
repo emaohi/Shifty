36317ab0cd2ed0e3c85c137f9a6dc6baa57279ac
rsegev <rsegev@agtinternational.com>
2017-11-27 17:22:38 +0200

    validate slot request is enabled in requestForm

--- core/forms.py
+++ core/forms.py
@@ -240,10 +240,16 @@ class SelectSlotsForm(forms.ModelForm):
         exclude = ('employee', 'submission_time')
 
     def __init__(self, *args, **kwargs):
-        business = kwargs.pop('business')
+        self.business = kwargs.pop('business')
         week = kwargs.pop('week')
         super(SelectSlotsForm, self).__init__(*args, **kwargs)
-        self.fields['requested_slots'].queryset = ShiftSlot.objects.filter(week=week, business=business).\
+        self.fields['requested_slots'].queryset = ShiftSlot.objects.filter(week=week, business=self.business).\
             exclude(is_mandatory=True)
         logger.info("query set is %s", self.fields['requested_slots'].queryset)
         self.fields['requested_slots'].widget.attrs['class'] = 'selectpicker'
+
+    def clean(self):
+        super(SelectSlotsForm, self).clean()
+
+        if not self.business.slot_request_enabled:
+            raise forms.ValidationError('Slots request is currently unavailable')
--- core/views.py
+++ core/views.py
@@ -4,7 +4,6 @@ import logging
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
 from django.utils import timezone
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseServerError, JsonResponse, \
     HttpResponseBadRequest
@@ -100,7 +99,7 @@ def broadcast_message(request):
         else:
             logger.error('broadcast form is not valid')
             form = BroadcastMessageForm()
-            messages.error(request, message='couldn\'t send broadcast message' % str(form.errors))
+            messages.error(request, message='couldn\'t send broadcast message: %s' % str(form.errors.as_text()))
             return HttpResponseRedirect('/')
     else:    # method is GET
         form = BroadcastMessageForm()
@@ -253,17 +252,19 @@ def submit_slots_request(request):
     curr_business = get_curr_business(request)
     if request.method == 'GET':
         form = SelectSlotsForm(business=curr_business, week=next_week_no)
-        url = reverse('slots_request')
-        return render(request, 'employee/slot_list.html', {'form': form, 'url': url})
-
+        return render(request, 'employee/slot_list.html', {'form': form})
     else:
         form = SelectSlotsForm(request.POST, business=curr_business, week=next_week_no)
-        shifts_request = save_shifts_request(form, request)
-        delete_other_requests(request, shifts_request)
+        if form.is_valid():
+            shifts_request = save_shifts_request(form, request)
+            delete_other_requests(request, shifts_request)
 
-        logger.info('slots chosen are: ' + str(shifts_request.requested_slots))
-        messages.success(request, 'request saved')
-        return HttpResponseRedirect('/')
+            logger.info('slots chosen are: ' + str(shifts_request.requested_slots))
+            messages.success(request, 'request saved')
+            return HttpResponseRedirect('/')
+        else:
+            messages.error(request, message='couldn\'t save slots request: %s' % str(form.errors.as_text()))
+            return render(request, 'employee/home.html', {'form': form})
 
 
 @login_required(login_url='/login')
