7e72e0e2b4bb73b833c4c031a07a71a3df0c2ae6
rsegev <rsegev@agtinternational.com>
2017-12-28 16:59:59 +0200

    fix pylint

--- menu/tests/test_helpers.py
+++ menu/tests/test_helpers.py
@@ -1,5 +1,12 @@
-from menu.models import Quiz
+from menu.models import Quiz, Question, Answer
+
+
+def create_answers(question):
+    for i in range(4):
+        Answer.objects.create(question=question, is_correct=True if i == 0 else False, content='')
 
 
 def create_quiz(business):
-    quiz = Quiz.objects.create()
+    quiz = Quiz.objects.create(business=business, name='test quiz')
+    question = Question.create(quiz=quiz, content='is this test question?')
+    # answers = create_answers(question)
